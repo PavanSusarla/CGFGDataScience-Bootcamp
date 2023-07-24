e9fbe867d2a88cdad1a85f88f6e4d243ff6a0022
Pavan Susarla <susarlapavab955@gmail.com>
2023-07-24 15:52:18 +0530

    numbers in decresing order

--- loops codes/characters at even indices.py
+++ loops codes/characters at even indices.py
@@ -2,4 +2,3 @@
 s = input().strip()
 for i in range(0, len(s), 2):  ## from 0 to length of str and skip 2
     print(s[i], end="")
-    
---
+++ loops codes/x to 0 in decreasing order in a single line..py
@@ -0,0 +1,5 @@
+#Given a number x, the task is to print the numbers from x to 0 in decreasing order in a single line.
+x = int(input())
+while (x >= 0):
+    print(x, end=" ")
+    x -= 1
