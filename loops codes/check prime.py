2ccd73b428a1d5f3a69017298103d4e941904ce0
Pavan Susarla <susarlapavab955@gmail.com>
2023-07-24 19:05:20 +0530

    prime

---
+++ loops codes/check prime.py
@@ -0,0 +1,13 @@
+#Given an integer n check if n is prime or not.
+#A prime number is a number that is divisible by 1 and itself only.
+def prime(n):
+    count=0
+    for i in range(2,n+1):
+        if n%i==0:
+            count+=1
+    if count==1:
+        return True
+    else:
+        return False
+n=int(input())
+print(prime(n))
