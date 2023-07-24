ad01959ae67db230b9368d94e12ff86bea093850
Pavan Susarla <susarlapavab955@gmail.com>
2023-07-24 18:32:22 +0530

    sum of n natural numbers

---
+++ loops codes/sum of n numbers.py
@@ -0,0 +1,8 @@
+#Given an integer N find the sum of the first N natural number.
+def nSum(n):
+    ans = 0
+    for i in range(1, n + 1):
+        ans += i
+    return ans
+n=int(input("Enter ther number"))
+print(nSum(n)
