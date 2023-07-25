2c583b855174d51d0a0fa2e8f74605410ee990d5
Pavan Susarla <susarlapavab955@gmail.com>
2023-07-25 10:57:44 +0530

    progressions

---
+++ operators/AP Term.py
@@ -0,0 +1,14 @@
+"""
+Given three integer, a,d and n. Where a is the first term, d is the common difference of an A.P. \n
+ Calculate the nth term of A.P.  The nth term is given by an = a + (n-1)d
+"""
+a = int(input())
+d = int(input())
+n = int(input())
+
+# Complete the code below
+ans = a + (n - 1) * d
+# Complete the code above
+
+# The line below prints the output. Don't change it!
+print(ans)
---
+++ operators/Evaluate Formulae.py
@@ -0,0 +1,15 @@
+"""Given four inputs that are stored in variables a,b,c, and d. \n
+You need to write an expression to evaluate the following formula. \n
+Use integer division. The expression should be a single statement.
+"""
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+# Complete the code below that stores the output of the expression
+res = ((a + b) // c) + d
+# Complete the code above that stores the output of the expression
+
+# This prints the output. Don't change it!
+print(res)
