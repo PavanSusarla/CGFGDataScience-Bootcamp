55fccd91778216afaafb1fef92b47deeb330ce7b
Pavan Susarla <susarlapavab955@gmail.com>
2023-07-25 11:36:35 +0530

    with arguments

---
+++ Functions/function with parameters.py
@@ -0,0 +1,5 @@
+"""Here two integers a and b are given. The take input and their values are passed as arguments to the function argumentFunction. \n
+The argumentFunction is responsible for the syntax print(a+b). You need to write this function."""
+def argumentFunction(a,b):
+    print(a+b)
+argumentFunction(2,3)
